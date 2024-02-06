0bba915be14c6004737d7ce41dadb4716742d68d
Hector Torres <htorres5@ucsc.edu>
2024-02-06 15:53:26 -0800

    added icons, updated data, default pointer

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import Button from "../Button";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 import yourData from "../../data/portfolio.json";
+
+library.add(fab, fas);
 
 const Socials = ({ className }) => {
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
         <Button key={index} onClick={() => window.open(social.link)}>
-          {social.title}
+          <FontAwesomeIcon icon={social.icon} pull="left"/>{" "}{social.title}
         </Button>
       ))}
     </div>
--- pages/index.js
+++ pages/index.js
@@ -91,7 +91,7 @@ export default function Home() {
             </h1>
           </div>
 
-          <Socials className="mt-2 laptop:mt-5" />
+          <Socials className="mt-2 ml-3 laptop:mt-5" />
         </div>
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0" ref={workRef}>
           <h1 className="text-2xl text-bold">Work.</h1>
